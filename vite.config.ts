5748fd35a4f6fa61e638e15e3d1682f29aee715b
lexi <tori@disroot.org>
2023-07-20 22:27:30 +0200

    add Autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		postcss: {
+			plugins: [
+				autoprefixer()
+			]
+		}
 	}
 });
