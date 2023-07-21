d1b203a84cd4b8ee6870c50b451aa498004a4d87
lexi <tori@disroot.org>
2023-07-21 16:18:21 +0200

    bump deps & refactor tests to be better

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import { devices } from '@playwright/test';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
@@ -5,7 +6,21 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'tests'
+	use: {
+		trace: 'on-first-retry',
+	},
+	testDir: './src/tests/e2e',
+	workers: "100%",
+	reporter: [
+		['html', {outputFolder: './test-results/playwright-report'}],
+		['list']
+	],
+	projects: [
+		{name: 'chromium', use: devices['Desktop Chrome']},
+		{name: 'firefox', use: devices['Desktop Firefox']},
+		{name: 'webkit', use: devices['Desktop Safari']}
+	],
+	outputDir: './test-results/playwright-output',
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,19 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: [
+			'./src/**/*.test.ts',
+			'./src/tests/unit/**/*.ts',
+		],
+		exclude: ['./src/tests/e2e/**/*'],
+		benchmark: {
+			outputFile: './test-results/vitest-benchmark.json',
+		},
+		outputFile: './test-results/vitest-report/index.html',
+		reporters: ['html', 'default'],
+		coverage: {
+			reportsDirectory: './test-results/vitest-coverage',
+		}
 	},
 	css: {
 		postcss: {
