dc95dac8b90261ea6dbd292fcb51384e1d309cab
lexi <tori@disroot.org>
2023-07-21 16:30:56 +0200

    add based opinions (use spaces instead of tabs)

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,22 +2,22 @@ import { devices } from '@playwright/test';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
-	use: {
-		trace: 'on-first-retry'
-	},
-	testDir: './src/tests/e2e',
-	workers: '100%',
-	reporter: [['html', { outputFolder: './test-results/playwright-report' }], ['list']],
-	projects: [
-		{ name: 'chromium', use: devices['Desktop Chrome'] },
-		{ name: 'firefox', use: devices['Desktop Firefox'] },
-		{ name: 'webkit', use: devices['Desktop Safari'] }
-	],
-	outputDir: './test-results/playwright-output'
+    webServer: {
+        command: 'npm run build && npm run preview',
+        port: 4173
+    },
+    use: {
+        trace: 'on-first-retry'
+    },
+    testDir: './src/tests/e2e',
+    workers: '100%',
+    reporter: [['html', { outputFolder: './test-results/playwright-report' }], ['list']],
+    projects: [
+        { name: 'chromium', use: devices['Desktop Chrome'] },
+        { name: 'firefox', use: devices['Desktop Firefox'] },
+        { name: 'webkit', use: devices['Desktop Safari'] }
+    ],
+    outputDir: './test-results/playwright-output'
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,22 @@ import { defineConfig } from 'vitest/config';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['./src/**/*.test.ts', './src/tests/unit/**/*.ts'],
-		exclude: ['./src/tests/e2e/**/*'],
-		benchmark: {
-			outputFile: './test-results/vitest-benchmark.json'
-		},
-		outputFile: './test-results/vitest-report/index.html',
-		reporters: ['html', 'default'],
-		coverage: {
-			reportsDirectory: './test-results/vitest-coverage'
-		}
-	},
-	css: {
-		postcss: {
-			plugins: [tailwindcss(), autoprefixer()]
-		}
-	}
+    plugins: [sveltekit()],
+    test: {
+        include: ['./src/**/*.test.ts', './src/tests/unit/**/*.ts'],
+        exclude: ['./src/tests/e2e/**/*'],
+        benchmark: {
+            outputFile: './test-results/vitest-benchmark.json'
+        },
+        outputFile: './test-results/vitest-report/index.html',
+        reporters: ['html', 'default'],
+        coverage: {
+            reportsDirectory: './test-results/vitest-coverage'
+        }
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), autoprefixer()]
+        }
+    }
 });
